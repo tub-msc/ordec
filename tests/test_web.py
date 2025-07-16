0cad1674fb7ed593e6bf00472b534452a0a4b1f6
Tobias Kaiser <kaiser@tu-berlin.de>
2025-07-16 15:31:04 +0200

    visual web testing using SSIM-PIL

--- tests/test_web.py
+++ tests/test_web.py
@@ -17,14 +17,26 @@ try:
     from selenium.webdriver.common.keys import Keys
     from selenium.webdriver.common.by import By
     from PIL import Image, ImageChops
+    from SSIM_PIL import compare_ssim
 except ImportError:
     skip_webtests = True
 else:
     skip_webtests = False
 
     webdriver_options = webdriver.ChromeOptions()
-    webdriver_options.add_argument("--headless=new")
-    webdriver_options.add_argument("--window-size=1280,720")
+    #webdriver_options.add_argument("--headless=new")
+    webdriver_options.add_argument("--force-device-scale-factor=1")
+    # The following sets the window size, not the viewport size (see resize_viewport below):
+    # webdriver_options.add_argument("--window-size=1280,720")
+
+
+examples = [
+    "nand2",
+    "voltagedivider_py",
+    "blank",
+    "voltagedivider",
+    "diffpair",
+]
 
 refdir = importlib.resources.files("tests.web_ref")
 
@@ -45,50 +57,45 @@ def webserver():
 @pytest.mark.web
 @pytest.mark.skipif(skip_webtests, reason="Prerequesites for web tests not installed.")
 def test_index(webserver):
-    driver = webdriver.Chrome(options=webdriver_options)
-    driver.get(webserver.url + '')
-    app_html_link_queries = set()
-    for a in driver.find_elements(By.TAG_NAME, 'a'):
-        href =urlparse(a.get_attribute('href'))
-        if href.path == '/app.html':
-            app_html_link_queries.add(href.query)
+    with webdriver.Chrome(options=webdriver_options) as driver:
+        driver.get(webserver.url + '')
+        app_html_link_queries = set()
+        for a in driver.find_elements(By.TAG_NAME, 'a'):
+            href =urlparse(a.get_attribute('href'))
+            if href.path == '/app.html':
+                app_html_link_queries.add(href.query)
 
-    assert app_html_link_queries == {
-        'example=nand2',
-        'example=voltagedivider_py',
-        'example=blank',
-        'example=voltagedivider',
-        'example=diffpair',
-    }
+    # Check that we link to each expected example.
+    assert app_html_link_queries == {f'example={example}' for example in examples}
 
-    driver.quit()
+def resize_viewport(driver, w, h):
+    w_overhead = driver.execute_script("return window.outerWidth - window.innerWidth;")
+    h_overhead = driver.execute_script("return window.outerHeight - window.innerHeight;")
+    driver.set_window_size(w+w_overhead, h+h_overhead)
 
-
-# TODO: The screenshot in test_screenshot_voltagedivider is not always the same.
 @pytest.mark.web
 @pytest.mark.skipif(skip_webtests, reason="Prerequesites for web tests not installed.")
-def test_screenshot_voltagedivider(webserver, tmp_path):
-    driver = webdriver.Chrome(options=webdriver_options)
-    driver.get(webserver.url + 'app.html?example=voltagedivider')
-    
-    # Hide ace-editor cursor (to always get the same screenshot):
-    driver.execute_script(
-        'document.styleSheets[0].insertRule(".ace_cursor { opacity: 0 !important; }", 0 )')
+@pytest.mark.parametrize('example', examples)
+def test_screenshot_example(webserver, tmp_path, example):
+    with webdriver.Chrome(options=webdriver_options) as driver:
+        resize_viewport(driver, 800, 600)
+        driver.get(webserver.url + f'app.html?example={example}')
+        
+        # Hide ace-editor cursor (to always get the same screenshot):
+        driver.execute_script(
+            'document.styleSheets[0].insertRule(".ace_cursor { opacity: 0 !important; }", 0 )')
 
-    # TODO: Delay to wait for result to show. We should do this somehow using JavaScript.
-
-    time.sleep(2)
-    
-    result_png = driver.get_screenshot_as_png()
-    (tmp_path / "voltagedivider.png").write_bytes(result_png) # Write output to tmp_path for user.
+        # TODO: Delay to wait for result to show. We should do this somehow using JavaScript.
+        time.sleep(2)
+        
+        result_png = driver.get_screenshot_as_png()
+        (tmp_path / f"{example}.png").write_bytes(result_png) # Write output to tmp_path for user.
+        #driver.save_screenshot('screenshot.png')
 
     result_image = Image.open(io.BytesIO(result_png))
-    
-    #x=driver.execute_script("return document.documentElement.outerHTML")
-    #Path("test.html").write_text(x)
 
-    driver.quit()
-
-    ref_image = Image.open(refdir / 'voltagedivider.png')
-    
-    assert not ImageChops.difference(result_image, ref_image).getbbox()
+    ref_image = Image.open(refdir / f'{example}.png')
+    # I am not sure whether SSIM is really appropriate for this, but it seems fine so far.
+    ssim = compare_ssim(result_image, ref_image, GPU=False)
+    print(f"ssim={ssim}")
+    assert ssim > 0.99
