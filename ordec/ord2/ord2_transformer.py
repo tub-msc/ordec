bd8b75546f4227e1fd1ed7eb5849f05cede12c17
schwimmbeck <dominik.schwimmbeck@tu-berlin.de>
2025-10-29 12:11:17 +0100

    Added ORD2 cell definition and rational numbers

--- ordec/ord2/ord2_transformer.py
+++ ordec/ord2/ord2_transformer.py
@@ -1,7 +1,34 @@
 # SPDX-FileCopyrightText: 2025 ORDeC contributors
 # SPDX-License-Identifier: Apache-2.0
 
+# standard imports
+import ast
+
+# ordec imports
 from .python_transformer import PythonTransformer
 
+
 class Ord2Transformer(PythonTransformer):
-    pass
+
+    def celldef(self, nodes):
+        cell_name = nodes[0]
+        suite = nodes[1]
+        base = ast.Name(id='Cell', ctx=ast.Load())
+
+        return ast.ClassDef(
+            name=cell_name,
+            bases=[base],
+            keywords=[],
+            body=suite,
+            decorator_list=[],
+            type_params=[]
+        )
+
+    def rational_number(self, nodes):
+        if len(nodes) > 2:
+            rational_number = ast.Constant(str(nodes[0].value) + str(nodes[2].value))
+        else:
+            rational_number = ast.Constant(str(nodes[0].value) + nodes[1])
+        return ast.Call(func=ast.Name(id="R", ctx=ast.Load()), args=[rational_number])
+
+    SI_SUFFIX = lambda self, token: token.value
