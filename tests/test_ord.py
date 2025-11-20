f5afb78c9f6d3c2637613d259d2703f992a509ca
schwimmbeck <dominik.schwimmbeck@tu-berlin.de>
2025-11-20 12:25:50 +0100

    restructured ord test files

--- tests/test_ord.py
+++ tests/test_ord.py
@@ -6,12 +6,12 @@ import ordec.importer
 from ordec.core import Cell
 
 def test_ord_empty():
-    from ordec.lib.ord_test import empty
+    from ordec.lib.ord1_test import empty
 
 def test_ord_empty2():
-    from ordec.lib.ord_test import empty2
+    from ordec.lib.ord1_test import empty2
 
 def test_ord_multicell():
-    from ordec.lib.ord_test import multicell
+    from ordec.lib.ord1_test import multicell
     assert issubclass(multicell.Cell1, Cell)
     assert issubclass(multicell.Cell2, Cell)
