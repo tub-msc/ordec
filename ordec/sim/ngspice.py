703932b840763ece836794581ce822362a3fa8e8
Tobias Kaiser <kaiser@tu-berlin.de>
2026-02-17 13:23:47 +0100

    removed ngspice.py

--- ordec/sim/ngspice.py
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: 2025 ORDeC contributors
-# SPDX-License-Identifier: Apache-2.0
-
-from .ngspice_subprocess import NgspiceSubprocess
-
-# Ngspice is now an alias of NgspiceSubprocess
-Ngspice = NgspiceSubprocess
