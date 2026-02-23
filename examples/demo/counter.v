30f34e03cd353f0af85222c75f3883fd54301360
Tobias Kaiser <kaiser@tu-berlin.de>
2026-02-23 11:32:28 +0100

    added examples/demo/counter_yosys.py

---
+++ examples/demo/counter.v
@@ -0,0 +1,25 @@
+module counter(
+    input wire clk_i,
+    input wire rst_ni,
+    input wire down_i,
+    input wire en_i,
+    output reg [15:0] val_o
+);
+
+    always @(posedge clk_i, negedge rst_ni) begin
+        if(~rst_ni) begin
+            val_o <= '0;
+        end
+        else begin
+            if(en_i) begin
+                if(down_i) begin
+                    val_o <= val_o - 1;
+                end
+                else begin
+                    val_o <= val_o + 1;
+                end
+            end
+        end
+    end
+
+endmodule
---
+++ examples/demo/counter_yosys.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+from notcl import TclTool
+from pathlib import Path
+import tempfile
+import json
+import os
+from typing import Iterable
+
+class Yosys(TclTool):
+    def cmdline(self):
+        return ["yosys", "-c", self.script_name()]
+
+
+def synthesize(source_files: Iterable[Path], top: str, lib: Path, enable_slang: bool=False) -> str:
+    """
+    Returns Verilog netlist as string.
+    """
+    with tempfile.TemporaryDirectory() as tmpdir:
+        with Yosys() as yosys:
+            yosys('yosys -import')
+            if enable_slang:
+                yosys.read_slang(*list(source_files), top=top)
+            else:
+                yosys.read_verilog(*list(source_files))
+            yosys.synth(top=top)
+            #yosys.hierarchy(top=top)
+            #yosys('yosys proc')
+            yosys.flatten()
+            yosys.opt()
+            yosys.dfflibmap(liberty=lib)
+            yosys.abc(liberty=lib)
+            yosys.splitnets()
+            yosys("yosys rename -hide */w:*\\[*")
+            yosys.opt_clean(purge=True)
+            yosys.write_verilog(Path(tmpdir) / 'out.v')
+        return (Path(tmpdir) / 'out.v').read_text()
+
+def read_verilog(verilog_str):
+    """
+    Returns unpacked JSON from Yosys.
+    """
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        (Path(tmpdir) / 'in.v').write_text(verilog_str)
+        with Yosys() as yosys:
+            yosys('yosys -import')
+            yosys.read_verilog(Path(tmpdir) / 'in.v')
+            yosys.write_json(Path(tmpdir) / 'out.json')
+        return json.loads((Path(tmpdir) / 'out.json').read_text())
+
+def main():
+    lib = Path(os.getenv('ORDEC_PDK_IHP_SG13G2')) / 'libs.ref/sg13g2_stdcell/lib/sg13g2_stdcell_typ_1p20V_25C.lib'
+    verilog = synthesize([Path(__file__).parent/'counter.v'], 'counter', lib)
+    print(verilog)
+    data = read_verilog(verilog)
+    #print(json.dumps(data, indent=2))
+    #for k, v in data['modules'].items():
+    #    print(k)
+
+if __name__ == "__main__":
+    main()
+
